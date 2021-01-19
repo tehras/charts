54f253e9cbde0033d5e95914bda2969dbff8ac19
Taras Koshkin <tkoshkin@squareup.com>
2021-01-18 16:47:22 -0800

    Trying to setup jitpack

--- lib/bar/build.gradle.kts
+++ lib/bar/build.gradle.kts
@@ -1,21 +1,23 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
+  id("com.android.library")
+  kotlin("android")
+  `maven-publish`
 }
 
 apply(from = rootProject.file("gradle/configure-android.gradle"))
 apply(from = rootProject.file("gradle/configure-compose.gradle"))
+apply(from = rootProject.file("gradle/jitpack-publish.gradle"))
 
 dependencies {
-    api(project(":lib:common"))
+  api(project(":lib:common"))
 
-    implementation(Kotlin.stdLib)
+  implementation(Kotlin.stdLib)
 
-    implementation(Compose.animation)
-    implementation(Compose.core)
-    implementation(Compose.layout)
-    implementation(Compose.foundation)
-    implementation(Compose.material)
-    implementation(Compose.runtime)
-    debugImplementation(Compose.tooling)
+  implementation(Compose.animation)
+  implementation(Compose.core)
+  implementation(Compose.layout)
+  implementation(Compose.foundation)
+  implementation(Compose.material)
+  implementation(Compose.runtime)
+  debugImplementation(Compose.tooling)
 }
--- lib/pie/build.gradle.kts
+++ lib/pie/build.gradle.kts
@@ -1,22 +1,24 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
+  id("com.android.library")
+  kotlin("android")
+  `maven-publish`
 }
 
 apply(from = rootProject.file("gradle/configure-android.gradle"))
 apply(from = rootProject.file("gradle/configure-compose.gradle"))
+apply(from = rootProject.file("gradle/jitpack-publish.gradle"))
 
 dependencies {
-    api(project(":lib:common"))
+  api(project(":lib:common"))
 
-    implementation(Kotlin.stdLib)
+  implementation(Kotlin.stdLib)
 
-    implementation(Compose.animation)
-    implementation(Compose.core)
-    implementation(Compose.layout)
-    implementation(Compose.foundation)
-    implementation(Compose.runtime)
+  implementation(Compose.animation)
+  implementation(Compose.core)
+  implementation(Compose.layout)
+  implementation(Compose.foundation)
+  implementation(Compose.runtime)
 
-    // Previews weren't working when using debugImplementation
-    implementation(Compose.tooling)
+  // Previews weren't working when using debugImplementation
+  implementation(Compose.tooling)
 }
