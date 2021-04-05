a05ee6707322bd9e417966e58f3ef82d9b1660ff
Noah Jutz <noahjutz@tutanota.de>
2021-04-05 14:25:01 +0200

    Upgrade compose to v1.0.0-beta03
    * activity-compose to v1.3.0-alpha05
    * kotlin to v1.4.31

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,9 +1,9 @@
 @file:JvmName("Deps")
 
 object Versions {
-    const val composeCompilerVersion = "1.4.30"
-    const val compose = "1.0.0-beta01"
-    const val kotlin = "1.4.30"
+    const val composeCompilerVersion = "1.4.31"
+    const val compose = "1.0.0-beta03"
+    const val kotlin = "1.4.31"
     const val targetSdk = 30
     const val buildVersion = "30.0.2"
 }
@@ -20,7 +20,7 @@ object Compose {
 }
 
 object Android {
-    const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha02"
+    const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha05"
     const val appcompat = "androidx.appcompat:appcompat:1.3.0-beta01"
 }
 
